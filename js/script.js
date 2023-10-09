783aef0af47c73f041e8381cd826ac51765fd8b7
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-09 16:38:18 +0200

    converted strings to numbers

--- js/script.js
+++ js/script.js
@@ -5,19 +5,24 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-let km = prompt('Quanti chilometri vuoi percorrere?');
+let km = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
 console.log(km)
 
-let età = prompt('Quanti anni hai?');
+let età = parseInt(prompt('Quanti anni hai?'));
 console.log(età)
 
 let prezzo = (0.21 * km)
 console.log(prezzo)
 
-if (età <= 18) {
+if (isNaN(età) | isNaN(km)){
+    window.location.reload();
+}
+if (età < 18) {
     prezzo = (prezzo - (prezzo* 0.20));
+    // prezzo.toPrecision(4);
     console.log('prezzo €', prezzo);
-} else if (età >= 65){
+} else if (età > 65){
     prezzo = (prezzo - (prezzo* 0.40));
+    // prezzo.toPrecision(4);
     console.log('prezzo €', prezzo);
 }
